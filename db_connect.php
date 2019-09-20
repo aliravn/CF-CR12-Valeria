5c4c8368403e191c45137aadae0e72b35ccb7f97
aliravn <alira.vn@gmail.com>
2019-09-20 15:04:09 +0200

    create connection to DB

---
+++ db_connect.php
@@ -0,0 +1,13 @@
+<?php
+// this will avoid mysql_connect() deprecation error.
+error_reporting( ~E_DEPRECATED & ~E_NOTICE );
+
+$connect = new mysqli ('localhost','root','Deadend6!','cr12_valeria_travelmatic');
+
+if ($connect->connect_errno) {
+   echo "Failed to connect to MySQL: " . $connect->connect_error;
+}
+else {echo "connected";}
+?>
+
+
