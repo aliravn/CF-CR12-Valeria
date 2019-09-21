a31647f325e9fc3eb6d5cfbc1a16e07d93ec5717
aliravn <alira.vn@gmail.com>
2019-09-21 13:54:45 +0200

    clean and optimize code

--- a_delete.php
+++ a_delete.php
@@ -4,28 +4,30 @@ session_start();
 if(!isset($_SESSION['user'])) {
 	header("Location: index.php");
 	exit;
-}
+} else {
+	require_once 'db_connect.php';
 
-require_once 'db_connect.php';
+	$sql_user = "SELECT * FROM users WHERE userID=".$_SESSION['user'];
+	$result = $connect->query($sql_user);
+	$user_details = mysqli_fetch_array($result, MYSQLI_ASSOC);
 
-$sql_user = "SELECT * FROM users WHERE userID=".$_SESSION['user'];
-$result = $connect->query($sql_user);
-$user_details = mysqli_fetch_array($result, MYSQLI_ASSOC);
-
-if ($user_details['userpriv']==1 && $_GET['id']) {
-	$id = $_GET['id'];
-	$sql_delete = "DELETE FROM posts WHERE postID = '$id'" ;
-
-	if($connect->query($sql_delete) === TRUE) {
+	if ($user_details['userpriv'] != 1) {
 		header("Location: home.php");
 		exit;
-	}
-} else if ($user_details['userpriv'] != 1) {
-	echo "you have no rights to perform this action";
-} else {
-	echo "Error while updating record : ". $connect->error;
-}
+	} else {
+		if ($_GET['id']) {
+		$id = $_GET['id'];
+		$sql_delete = "DELETE FROM posts WHERE postID = '$id'" ;
 
+			if($connect->query($sql_delete) === TRUE) {
+				header("Location: home.php");
+				exit;
+			} else {
+				echo "Error while updating record : ". $connect->error;
+			}
+		}
+	}
+}			
 $connect->close();
 ?>
 
