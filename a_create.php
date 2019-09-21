dedfcd4de8e50bb3ef0de3052d7134c18317715c
aliravn <alira.vn@gmail.com>
2019-09-21 13:31:02 +0200

    clean and optimize the code

--- a_create.php
+++ a_create.php
@@ -3,17 +3,18 @@ session_start();
 if(!isset($_SESSION['user'])) {
 	header("Location: index.php");
 	exit;
-}
+} else {
+	require_once 'db_connect.php';
 
-require_once 'db_connect.php';
+	$sql_user = "SELECT * FROM users WHERE userID=".$_SESSION['user'];
+	$result = $connect->query($sql_user);
+	$user_details = mysqli_fetch_array($result, MYSQLI_ASSOC);
 
-$sql_user = "SELECT * FROM users WHERE userID=".$_SESSION['user'];
-$result = $connect->query($sql_user);
-$user_details = mysqli_fetch_array($result, MYSQLI_ASSOC);
-
-if ($user_details['userpriv'] == 1) {
-
-	if ($_POST) {
+	if ($user_details['userpriv'] != 1) {
+		header("Location: home.php");
+		exit;
+	} else {
+		if ($_POST) {
 		$post_title = $_POST['name'];
 		$post_location = $_POST['fk_location'];
 		$post_image = $_POST['image'];
@@ -27,8 +28,8 @@ if ($user_details['userpriv'] == 1) {
 
 		$sql_request = "INSERT INTO posts (`name`, `fk_location`, `image`, `homepage`, `type`, `poi_type`, `phone`, `description`, `event_when`, `price`) VALUES ('$post_title', '$post_location', '$post_image', '$post_homepage', '$post_type', '$post_poi_type', '$post_phone', '$post_description', '$post_datetime', '$post_price')";
 
-		if($connect->query($sql_request) === TRUE) {
-			header("Location: home.php");
+			if($connect->query($sql_request) === TRUE) {
+				header("Location: home.php");
 
 			// echo "
 			// <div class='confirmation-message'>
@@ -37,14 +38,11 @@ if ($user_details['userpriv'] == 1) {
 			// 	<a href='index.php'><button type='button'>Go Home</button></a>
 			// </div>
 			// ";
-		} else {
-			echo "Error while updating record : ". $connect->error;
+			} else {
+				echo "Error while updating record : ". $connect->error;
+			}
 		}
 	}
-} else if ($user_details['userpriv'] == 1) {
-	echo "you have no rights to perform this action";
-} else {
-	echo "smth went wrong";
 }
 
 $connect->close();
--- a_update.php
+++ a_update.php
@@ -3,16 +3,18 @@ session_start();
 if(!isset($_SESSION['user'])) {
 	header("Location: index.php");
 	exit;
-}
+} else {
+	require_once 'db_connect.php';
 
-require_once 'db_connect.php';
+	$sql_user = "SELECT * FROM users WHERE userID=".$_SESSION['user'];
+	$result = $connect->query($sql_user);
+	$user_details = mysqli_fetch_array($result, MYSQLI_ASSOC);
 
-$sql_user = "SELECT * FROM users WHERE userID=".$_SESSION['user'];
-$result = $connect->query($sql_user);
-$user_details = mysqli_fetch_array($result, MYSQLI_ASSOC);
-
-if ($user_details['userpriv'] == 1) {
-	if ($_POST) {
+	if ($user_details['userpriv'] != 1) {
+		header("Location: home.php");
+		exit;
+	} else {
+		if ($_POST) {
 		$id = $_POST['postID'];
 		$post_title = $_POST['name'];
 		$post_location = $_POST['fk_location'];
@@ -27,8 +29,8 @@ if ($user_details['userpriv'] == 1) {
 
 		$sql_request = "UPDATE posts SET `name` = '$post_title', `fk_location` = '$post_location', `image` = '$post_image', `homepage` = '$post_homepage', `type` = '$post_type', `poi_type` = '$post_poi_type', `phone` = '$post_phone', `description` = '$post_description', `event_when` = '$post_datetime', `price` = '$post_price' WHERE postID = '$id'";
 
-		if($connect->query($sql_request) === TRUE) {
-			header("Location: home.php");
+			if($connect->query($sql_request) === TRUE) {
+					header("Location: home.php");
 
 			// echo "
 			// <div class='confirmation-message'>
@@ -37,16 +39,12 @@ if ($user_details['userpriv'] == 1) {
 			// 	<a href='index.php'><button type='button'>Go Home</button></a>
 			// </div>
 			// ";
-		} else {
-			echo "Error while updating record : ". $connect->error;
+			} else {
+				echo "Error while updating record : ". $connect->error;
+			}
 		}
 	}
-} else if ($user_details['userpriv'] == 1) {
-	echo "you have no rights to perform this action";
-} else {
-	echo "smth went wrong";
 }
-
 $connect->close();
 
 ?>
